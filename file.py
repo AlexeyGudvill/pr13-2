61327b69ae97867dd43793f7651256c84b189136
Gudvill AM <agoodwill04@gmail.com>
2025-12-04 20:47:53 +0500

    Initial commit

---
+++ file.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    print("Функция сложения")
+    return a + b
+
+def multiply(a, b):
+    print("Функция умножения")
+    return a * b
+
+def subtract(a, b):
+    print("Функция вычитания")
+    return a - b
+
+def division(a, b):
+    print("Функция деления")
+    return a / b
---
+++ test_file.py
@@ -0,0 +1,7 @@
+def test_add(): assert 2 + 2 == 4
+
+def test_multiply(): assert 2 * 2 == 4
+
+def test_subtract(): assert 2 - 2 == 0
+
+def test_division(): assert 2 / 2 == 1
